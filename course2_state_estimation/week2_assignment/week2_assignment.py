66e15d2a2e04fc33db9935e1679f30d429dc846d
Adam Clare <adam.jarrett.clare@gmail.com>
2019-10-20 09:37:28 -0600

    Prediction step only

--- course2_state_estimation/week2_assignment/week2_assignment.py
+++ course2_state_estimation/week2_assignment/week2_assignment.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+from math import *
 
 def wraptopi(x):
     if x > np.pi:
@@ -22,7 +23,7 @@ def measurement_update(lk, rk, bk, P_check, x_check):
     return x_check, P_check
 
 
-with open('data.pickle', 'rb') as f:
+with open('week2_assignment/data.pickle', 'rb') as f:
     data = pickle.load(f)
 
 t = data['t']  # timestamps [s]
@@ -54,15 +55,28 @@ P_est = np.zeros([len(v), 3, 3])  # state covariance matrices
 
 x_est[0] = np.array([x_init, y_init, th_init]) # initial state
 P_est[0] = np.diag([1, 1, 0.1]) # initial state covariance
+# Initialze the state uncertainity matrix
+P_check = P_est[0]
 
 for k in range(1, len(t)):  # start at 1 because we've set the initial prediciton
 
-    
     delta_t = t[k] - t[k - 1]  # time step (difference between timestamps)
     
     # 1. Update state with odometry readings (remember to wrap the angles to [-pi,pi])
-    x_check = np.zeros(3)
-
+    # This is the state vector at the previous step
+    x_check = x_est[k - 1].reshape(3, 1)
+    # Propogate the state forward to the current step, first build the motion
+    # model matrix, phi.
+    # x_check[2] is our best estimate of theta, or theta at (k - 1)
+    phi = np.array([
+        [cos(x_check[2]) * delta_t, 0],
+        [sin(x_check[2]) * delta_t, 0],
+        [0, delta_t]])
+    # According to the equations provided we need to use v_k and w_k, NOT
+    # k-1
+    odom = np.array([v[k], om[k]])
+    x_check = x_check + phi @ odom.reshape(2, 1)
+    
     # 2. Motion model jacobian with respect to last state
     F_km = np.zeros([3, 3])
 
@@ -72,8 +86,8 @@ for k in range(1, len(t)):  # start at 1 because we've set the initial predicito
     # 4. Propagate uncertainty
 
     # 5. Update state estimate using available landmark measurements
-    for i in range(len(r[k])):
-        x_check, P_check = measurement_update(l[i], r[k, i], b[k, i], P_check, x_check)
+    # for i in range(len(r[k])):
+    #     x_check, P_check = measurement_update(l[i], r[k, i], b[k, i], P_check, x_check)
 
     # Set final state predictions for timestep
     x_est[k, 0] = x_check[0]
@@ -87,6 +101,7 @@ ax.plot(x_est[:, 0], x_est[:, 1])
 ax.set_xlabel('x [m]')
 ax.set_ylabel('y [m]')
 ax.set_title('Estimated trajectory')
+plt.savefig('images/week2_trajectory_xy_no_update_step.png', dpt=300)
 plt.show()
 
 e_fig = plt.figure()
@@ -95,6 +110,7 @@ ax.plot(t[:], x_est[:, 2])
 ax.set_xlabel('Time [s]')
 ax.set_ylabel('theta [rad]')
 ax.set_title('Estimated trajectory')
+plt.savefig('images/week2_trajectory_time_no_update_step.png', dpt=300)
 plt.show()
 
 with open('submission.pkl', 'wb') as f:
